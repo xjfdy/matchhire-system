a6fbe27e06d7d7f3b205d1b11707f651cc52551d
xjfdy <xjfdy17@gmail.com>
2026-07-07 16:17:07 -0400

    feat: publish application-events to Kafka on submission and status change

--- application-service/src/main/java/com/matchhire/applicationservice/event/ApplicationEventProducer.java
+++ application-service/src/main/java/com/matchhire/applicationservice/event/ApplicationEventProducer.java
@@ -1,4 +1,48 @@
 package com.matchhire.applicationservice.event;
 
+import com.matchhire.applicationservice.model.Application;
+import com.matchhire.applicationservice.model.ApplicationStatus;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
 public class ApplicationEventProducer {
+
+    private static final String TOPIC = "application-events";
+
+    private final KafkaTemplate<String, ApplicationEvent> kafkaTemplate;
+
+    public ApplicationEventProducer(KafkaTemplate<String, ApplicationEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void publishSubmitted(Application application) {
+        ApplicationEvent event = new ApplicationEvent();
+        event.setEventType("SUBMITTED");
+        event.setApplicationId(application.getId());
+        event.setCandidateId(application.getCandidateId());
+        event.setJobId(application.getJobId());
+        event.setEmployerId(application.getEmployerId());
+        event.setNewStatus(application.getStatus());
+        event.setTimestamp(LocalDateTime.now());
+
+        kafkaTemplate.send(TOPIC, application.getId().toString(), event);
+    }
+
+    public void publishStatusChanged(Application application, ApplicationStatus oldStatus) {
+        ApplicationEvent event = new ApplicationEvent();
+        event.setEventType("STATUS_CHANGED");
+        event.setApplicationId(application.getId());
+        event.setCandidateId(application.getCandidateId());
+        event.setJobId(application.getJobId());
+        event.setEmployerId(application.getEmployerId());
+        event.setOldStatus(oldStatus);
+        event.setNewStatus(application.getStatus());
+        event.setTimestamp(LocalDateTime.now());
+
+        kafkaTemplate.send(TOPIC, application.getId().toString(), event);
+    }
+
 }
