5dd81e09497733c5d396bc1fe550ecaed887601e
xjfdy <xjfdy17@gmail.com>
2026-09-12 10:00:11 -0400

    fix

--- job-service/src/main/java/com/matchhire/jobservice/specification/JobSpecifications.java
+++ job-service/src/main/java/com/matchhire/jobservice/specification/JobSpecifications.java
@@ -1,4 +1,33 @@
 package com.matchhire.jobservice.specification;
 
-public class JobsSpecification {
+import com.matchhire.jobservice.model.JobType;
+import com.matchhire.jobservice.model.Jobs;
+import org.springframework.data.jpa.domain.Specification;
+
+public class JobSpecifications {
+
+    public static Specification<Jobs> hasTitleContaining(String title) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.lower(root.get("title")), "%" + title.toLowerCase() + "%");
+    }
+
+    public static Specification<Jobs> hasJobType(JobType jobType) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("jobType"), jobType);
+    }
+
+    public static Specification<Jobs> hasRemote(Boolean remote) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("remote"), remote);
+    }
+
+    public static Specification<Jobs> hasMinSalaryGreaterThanOrEqual(int minSalary) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.greaterThanOrEqualTo(root.get("minSalary"), minSalary);
+    }
+
+    public static Specification<Jobs> hasMaxSalaryLessThanOrEqual(int maxSalary) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.lessThanOrEqualTo(root.get("maxSalary"), maxSalary);
+    }
 }
